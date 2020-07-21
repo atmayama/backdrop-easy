aa84fe097f5592f12b177e814f962daaa545eb9a
Sushanth Kille <sushanthkille@gmail.com>
2020-07-21 19:33:48 +0530

    Updates

---
+++ precache-manifest.b4611e8983e02f4555490bae206c5219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ca36e61940f799230fe29b6662b824",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c568bca3506b9f6c5b8e",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "7fa2cbcc7f48879d15d6",
+    "url": "./static/js/2.53173f35.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.53173f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c568bca3506b9f6c5b8e",
+    "url": "./static/js/main.f6dc0a2a.chunk.js"
+  },
+  {
+    "revision": "b3b3abd0e445f7ebda27",
+    "url": "./static/js/runtime-main.2cbf5703.js"
+  }
+]);
