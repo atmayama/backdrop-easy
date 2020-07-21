b50dc9d2738b780a78ab6b5f270b91b7108624f6
Sushanth Kille <sushanthkille@gmail.com>
2020-07-21 18:51:07 +0530

    v0.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useContext } from 'react'
+import {CircularProgress, Backdrop, makeStyles} from '@material-ui/core';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const BackdropContext = React.createContext('backdrop-context');
+
+const useStyles = makeStyles((theme) => ({
+  backdropLight: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#000',
+  },backdropDark: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
+
+export const BackdropProvider = ({theme,progress,children}={theme:'light',progress:<CircularProgress/>}) => {
+  const [open,setOpen]=useState(false);
+
+  const classes = useStyles();
+
+  const openBackdrop = ()=>{
+    setOpen(true);
+  }
+  const closeBackdrop = ()=>{
+    setOpen(false);
+  }
+  return (
+  <BackdropContext.Provider value={[openBackdrop,closeBackdrop]}>
+    {children}
+    <Backdrop className={theme='light'?classes.backdropLight:classes.backdropDark} open={open} onClick={closeBackdrop}>
+        {progress}
+      </Backdrop>
+  </BackdropContext.Provider>
+  );
 }
+
+export const useBackdrop = ()=> useContext(BackdropContext);
